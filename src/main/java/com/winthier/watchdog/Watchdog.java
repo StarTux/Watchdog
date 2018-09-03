38f311eecc3815fbfcf0342cf06a69dd523da91a
StarTux <ths.loy@googlemail.com>
2018-09-03 19:24:55 +0200

    Clean up

--- src/main/java/com/winthier/watchdog/Watchdog.java
+++ src/main/java/com/winthier/watchdog/Watchdog.java
@@ -1,44 +1,42 @@
 package com.winthier.watchdog;
 
 import java.io.File;
-import java.io.IOException;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.PermissionDefault;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.command.Command;
+import org.bukkit.plugin.java.annotation.command.Commands;
+import org.bukkit.plugin.java.annotation.permission.Permission;
+import org.bukkit.plugin.java.annotation.permission.Permissions;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Description;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.Website;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
-public abstract class Watchdog {
-    int pid = 0;
-    volatile int idleSeconds;
-    volatile boolean cancelled = false;
+@Plugin(name = "Watchdog", version = "0.1")
+@Description("Stop the server when it hangs")
+@ApiVersion(ApiVersion.Target.v1_13)
+@Author("StarTux")
+@Website("https://cavetale.com")
+@Commands(@Command(name = "watchdog",
+                   desc = "Admin interface",
+                   aliases = {},
+                   permission = "watchdog.watchdog",
+                   usage = "/watchdog stop\n/watchdog tick\n/watchdog start"))
+@Permissions(@Permission(name = "watchdog.watchdog",
+                         desc = "Use /watchdog",
+                         defaultValue = PermissionDefault.OP))
+public final class Watchdog extends JavaPlugin implements Runnable {
+    private int pid;
+    private volatile int idleSeconds;
+    private volatile boolean disableWatchdog;
     static final int LIMIT = 120;
 
-    protected abstract void shutdown();
-    protected abstract void command(String command);
-
-    public void somethingHappened() {
-        idleSeconds = 0;
-    }
-
-    public void cancel() {
-        cancelled = true;
-    }
-
-    void info(String message) {
-        System.out.println("[Watchdog] " + message);
-    }
-    
-    void warn(String message) {
-        System.err.println("[Watchdog] " + message);
-    }
-
-    void logCommand(String command) {
-        info("Running command: " + command);
-        command(command);
-    }
-    
-    void logExec(String command) throws Exception {
-        info("Running process: " + command);
-        Runtime.getRuntime().exec(command);
-    }
-
-    public void enable() {
+    @Override
+    public void onEnable() {
+        getServer().getScheduler().runTaskTimer(this, () -> idleSeconds = 0, 20L, 20L);
         try {
             String[] path = new File("/proc/self").getCanonicalPath().split(File.separator);
             pid = Integer.parseInt(path[path.length - 1]);
@@ -47,24 +45,78 @@ public abstract class Watchdog {
             warn("Cannot parse pid");
             e.printStackTrace();
         }
-        new Thread() {
-            @Override public void run() {
-                info("Watchdog thread started");
-                while (true) {
-                    try {
-                        if (!loop()) break;
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-                info("Watchdog thread stopped");
-            }
-        }.start();
+        new Thread(this).start();
     }
 
-    boolean loop() throws Exception {
-        Thread.sleep(1000L);
-        switch (idleSeconds++) {
+    @Override
+    public void run() {
+        info("Watchdog thread started");
+        while (!disableWatchdog) {
+            try {
+                loop();
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+        info("Watchdog thread stopped");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (args.length != 1) return false;
+        switch (args[0]) {
+        case "stop":
+            disableWatchdog = true;
+            sender.sendMessage("Watchdog thread stopped");
+            break;
+        case "tick":
+            idleSeconds = 0;
+            sender.sendMessage("Watchdog thread ticked");
+            break;
+        case "start":
+            if (disableWatchdog) {
+                disableWatchdog = false;
+                new Thread(this).start();
+                sender.sendMessage("Watchdog thread started again");
+            } else {
+                sender.sendMessage("Watchdog already running");
+            }
+            break;
+        default:
+            return false;
+        }
+        return true;
+    }
+
+    void info(String message) {
+        System.out.println("[winthier.Watchdog] " + message);
+    }
+
+    void warn(String message) {
+        System.err.println("[winthier.Watchdog] " + message);
+    }
+
+    void logCommand(String command) {
+        info("Running command: " + command);
+        Bukkit.getScheduler().runTask(this, () -> {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+            });
+    }
+
+    void logExec(String command) throws Exception {
+        info("Running process: " + command);
+        Runtime.getRuntime().exec(command);
+    }
+
+    void loop() throws Exception {
+        final int seconds = idleSeconds;
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        idleSeconds += 1;
+        switch (seconds) {
         case 60:
             warn("Nothing happened for a minute. Starting to worry.");
             break;
@@ -76,10 +128,14 @@ public abstract class Watchdog {
             System.exit(1);
             break;
         case LIMIT + 65:
-            logExec("kill " + pid);
+            if (pid > 0) {
+                logExec("kill " + pid);
+            }
             break;
         case LIMIT + 70:
-            logExec("kill -KILL " + pid);
+            if (pid > 0) {
+                logExec("kill -KILL " + pid);
+            }
             break;
         case LIMIT + 75:
             logExec("killall java");
@@ -89,8 +145,8 @@ public abstract class Watchdog {
             break;
         case LIMIT + 90:
             warn("I give up.");
-            return false;
+        default:
+            break;
         }
-        return !cancelled;
     }
 }
